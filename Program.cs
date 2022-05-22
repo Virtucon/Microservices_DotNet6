d3f38cf1284b23c220eb72edeec362cdef5fd260
Conor <Conor@DESKTOP-HASRMAB>
2022-05-22 17:51:56 +0100

    Platform Dtos

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDataba
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
